f5e59fa3e5e6bc2fd795c776ee7418e032df08bd
Jean Klod Vidal <diklod.qi@gmail.com>
2024-03-05 10:39:46 -0300

    SQL Server adaptation and some test migrations

---
+++ SalesWebMvc/Migrations/20240304172850_Update1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class Update1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "Department",
+                newName: "Nome");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Nome",
+                table: "Department",
+                newName: "Name");
+        }
+    }
+}
